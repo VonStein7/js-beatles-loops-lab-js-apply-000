d25c4c839b20ee20a4412010a34a1836a18cb2f8
brenden williams <brendenw7@gmail.com>
2018-12-14 02:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const lennonFacts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
+
+const theBeatlesPlay = (arr1, arr2) => {
+  let newArr = [];
+  
+  arr1.forEach((e, idx)=>{
+    newArr.push(e + ' plays ' + arr2[idx]);
+  })
+  
+  return newArr;
+}
+
+const johnLennonFacts = (arr1) => {
+  let newArr = [];
+  
+  arr1.forEach(e=>{
+    newArr.push(e+"!!!");
+  })
+  
+  return newArr;
+}
+
+const iLoveTheBeatles = (loopCount) => {
+  let newArr = [];
+  let count = 0;
+  
+  do{
+    newArr.push('I love the Beatles!');
+    count++;
+  }while(loopCount <= 15)
+  
+  return newArr;
+}
